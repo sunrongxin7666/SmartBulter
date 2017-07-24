a8fb0849ba0774c794415f9e400662dd799dc9d2
Rongxin Sun <sunrongxin7666@163.com>
2017-07-24 22:08:55 +0800

    user setting

---
+++ app/src/main/java/com/android/srx/github/smartbulter/ui/PhoneActivity.java
@@ -0,0 +1,15 @@
+package com.android.srx.github.smartbulter.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.srx.github.smartbulter.R;
+
+public class PhoneActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_phone);
+	}
+}
--- app/src/main/java/com/android/srx/github/smartbulter/utils/UtilTools.java
+++ app/src/main/java/com/android/srx/github/smartbulter/utils/UtilTools.java
@@ -1,9 +1,15 @@
 package com.android.srx.github.smartbulter.utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Base64;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Project: SmartBulter
@@ -20,5 +26,19 @@ public class UtilTools {
 		Typeface fromAsset = Typeface.createFromAsset(context.getAssets(), "fronts/FONT.TTF");
 		//设置字体
 		((TextView)view).setTypeface(fromAsset);
+	}
+
+	//保存图片到shareutils
+	public static void putImageToShare(Context mContext, ImageView imageView) {
+		BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+		Bitmap bitmap = drawable.getBitmap();
+		//第一步：将Bitmap压缩成字节数组输出流
+		ByteArrayOutputStream byStream = new ByteArrayOutputStream();
+		bitmap.compress(Bitmap.CompressFormat.PNG, 80, byStream);
+		//第二步：利用Base64将我们的字节数组输出流转换成String
+		byte[] byteArray = byStream.toByteArray();
+		String imgString = new String(Base64.encodeToString(byteArray, Base64.DEFAULT));
+		//第三步：将String保存shareUtils
+		SharedUtils.putString(mContext, "image_title", imgString);
 	}
 }
